eaf051fb18e477834196d95715a958e547ddcdd0
RenGVZ <gaving@live.com>
2022-12-16 00:45:36 +0900

    changed contact behavior

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -17,34 +17,62 @@ const Footer = () => {
     setFormData({ ...formData, [name]: value })
   }
 
-  const handleSubmit = () => {
-    setLoading(true)
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // setLoading(true)
 
-    const contact = {
-      _type: 'contact',
-      name: name,
-      email: email,
-      message: message,
-    }
+    fetch('https://formsubmit.co/gavin.rene.g@gmail.com', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      }
+    }).then(() => {
+      // setLoading(false)
+      // setIsFormSubmitted(true)
+    })
+    .catch((err) => console.log('error:', err))
 
-    client.create(contact)
-      .then(() => {
-        setLoading(false)
-        setIsFormSubmitted(true)
-      })
-      .catch((err) => console.log(err));
+    // const contact = {
+    //   _type: 'contact',
+    //   name: name,
+    //   email: email,
+    //   message: message,
+    // }
+
+    // client.create(contact)
+    //   .then(() => {
+    //     setLoading(false)
+    //     setIsFormSubmitted(true)
+    //   })
+    //   .catch((err) => console.log(err));
+  }
+
+  const handleForm = (e) => {
+    e.preventDefault()
+
+    fetch('https://formsubmit.co/gavin.rene.g@gmail.com', {
+      method: 'POST',
+      data: {
+        _captcha: false,
+        _next: 'http://localhost:5173',
+        name: name,
+        email: email,
+        message: message
+      }
+    })
+    console.log('hey');
   }
 
   return (
     <>
       <h2 className='head-text'>Let's get in touch!</h2>
       <div className='app__footer-cards'>
-        <div className='app__footer-card'>
+        {/* <div className='app__footer-card'>
           <img src={images.email} alt='email' />
           <a href='mailto:gavin.rene.g@gmail.com' className='p-text'>
             gavin.rene.g@gmail.com
           </a>
-        </div>
+        </div> */}
         {/* <div className='app__footer-card'>
           <img src={images.mobile} alt='mobile' />
           <a href='tel:+81 070 4479 6011' className='p-text'>
@@ -53,8 +81,15 @@ const Footer = () => {
         </div> */}
 
         {!isFormSubmitted ? (
-          <div className='app__footer-form app__flex'>
+          <form
+            // onSubmit={handleForm}
+            action='https://formsubmit.co/gavin.rene.g@gmail.com'
+            method='POST'
+            className='app__footer-form app__flex'
+          >
             <div className='app__flex'>
+              <input type="hidden" name="_captcha" value="false" />
+              <input type="hidden" name="_next" value='http://localhost:5173/#contact'></input>
               <input
                 type='text'
                 className='p-text'
@@ -66,6 +101,7 @@ const Footer = () => {
             </div>
             <div className='app__flex'>
               <input
+                required
                 type='email'
                 className='p-text'
                 placeholder='Your email'
@@ -76,6 +112,8 @@ const Footer = () => {
             </div>
             <div>
               <textarea
+                required
+                type='text'
                 className='p-text'
                 placeholder='Your message'
                 value={message}
@@ -83,10 +121,10 @@ const Footer = () => {
                 onChange={handleChangeInput}
               />
             </div>
-            <button type='button' className='p-text' onClick={handleSubmit}>
+            <button type='submit' className='p-text'>
               {loading ? 'Sending...' : 'Send Message'}
             </button>
-          </div>
+          </form>
         ) : (
           <div>
             <h3 className='head-text'>Thanks for getting in touch!</h3>
