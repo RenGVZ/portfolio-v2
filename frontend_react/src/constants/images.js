dd67bd4cc5b3a421fbdb07fa35b95057a9e8f1ce
RenGVZ <gaving@live.com>
2022-12-18 22:37:58 +0900

    asset replacement

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -28,7 +28,7 @@ import about04 from '../assets/about04.png';
 import profile from '../assets/profile.png';
 import profile2 from '../assets/profile-3.png';
 import circle from '../assets/circle.svg';
-import logo from '../assets/site-logo-v5.png';
+import logo from '../assets/next-logo.png';
 
 import adidas from '../assets/adidas.png';
 import amazon from '../assets/amazon.png';
